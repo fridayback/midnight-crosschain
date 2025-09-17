96c902bdb058db2e1113bd07be1893d8f63eecc1
liulin <blue-sky-dl5@163.com>
2025-09-17 11:45:41 +0800

    alpha0.6.1

--- dist/witnesses.js
+++ dist/witnesses.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  * @Author: liulin
  * @Date: 2025-06-20 11:21:39
@@ -6,8 +7,11 @@
  * @FilePath: /midnight-crosschain/contract/src/witnesses.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-export const createCrossChainPrivateState = () => ({});
-export const witnesses = {
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.witnesses = exports.createCrossChainPrivateState = void 0;
+const createCrossChainPrivateState = () => ({});
+exports.createCrossChainPrivateState = createCrossChainPrivateState;
+exports.witnesses = {
 // TODO: Add witnesses
 };
 //# sourceMappingURL=witnesses.js.map
