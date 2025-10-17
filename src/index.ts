6d79dd6a29a463824d10ed8451ecd76afb9e0164
liulin <blue-sky-dl5@163.com>
2025-10-17 11:06:30 +0800

    bugfix:0.10.3

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: liulin 
  * @Date: 2025-06-20 12:02:08
  * @LastEditors: liulin blue-sky-dl5@163.com
- * @LastEditTime: 2025-10-17 10:43:17
+ * @LastEditTime: 2025-10-17 11:05:47
  * @FilePath: /midnight-crosschain/contract/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -537,7 +537,7 @@ export class CrossChainApi {
       return { uniqueId: uniqueId_0, coinIndex: coinIndex_0 };
     });
     assert(uniqueIds_0.length <= 5 && uniqueIds_0.length > 0, `uniqueIds must be between 1 and 5`);
-    for (let index = uniqueIds_0.length - 1; index < 5; index++) {
+    for (let index = uniqueIds_0.length; index < 5; index++) {
       uniqueIds_0.push({ uniqueId: Buffer.alloc(32), coinIndex: BigInt(0) });
     }
 
