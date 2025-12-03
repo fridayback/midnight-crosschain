21fa0952e452272bf21b0d3b4552901509bca1f0
liulin <blue-sky-dl5@163.com>
2025-12-03 10:56:59 +0800

    fix getUnVotedCrossProposal

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: liulin 
  * @Date: 2025-06-20 12:02:08
  * @LastEditors: liulin blue-sky-dl5@163.com
- * @LastEditTime: 2025-12-03 10:48:48
+ * @LastEditTime: 2025-12-03 10:56:12
  * @FilePath: /midnight-crosschain/contract/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -446,6 +446,7 @@ export class CrossChainApi {
     } else {
       voterPK = fromHex(this.providers.walletProvider.coinPublicKey);
     }
+    if(!this.isVoter(ledger,voter)) return [];
     const voterIndex = ledger.smgTxSigners.lookup({ bytes: voterPK });
     let res = [];
     for (const [uniquId, _] of ledger.crossProposal) {
